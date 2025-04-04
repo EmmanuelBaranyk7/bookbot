397ed07c093cb7b386c97c936188368a45227657
EmmanuelBaranyk <emmanuelbaranyk4@gmail.com>
2025-04-04 17:58:00 -0400

    completed bookbot

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-from stats import num_words
-from stats import char_count
+from stats import (
+    get_num_words,
+    char_count_dict,
+    chars_dict_to_sorted_list
+)
+
+def main():
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = char_count_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
+        return f.read()
 
 
-def main():
+def print_report(book_path, num_words, chars_sorted_list):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print(f"Found {get_num_words(get_book_text(book_path))} total words")
     print("--------- Character Count -------")
-    print(char_count(get_book_text('books/frankenstein.txt')))    
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,11 @@
-def num_words(text):
+def get_num_words(text):
     return len(text.split())
 
-def char_count(text):
+def char_count_dict(text):
     count = {}
     text = text.lower()
 
     for symbol in text:
-        if symbol.isalpha():
             if symbol in count:
                 count[symbol] += 1
             else:
@@ -14,3 +13,14 @@ def char_count(text):
 
     return count
 
+
+def sort_dict(dict):
+    return dict["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_dict)
+    return sorted_list
